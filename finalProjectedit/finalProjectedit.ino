4397baaabe0849f65669117225fb5c3165b3ff10
khadija-kamal <hashadkhadija942@gmail.com>
2023-05-02 01:52:13 +0200

    Edit mpu in different position & adding interrupts - works fine only sms need to be debug

--- finalProjectedit/finalProjectedit.ino
+++ finalProjectedit/finalProjectedit.ino
@@ -2,18 +2,18 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
-#define buttonPin2 5
-#define buttonPin1 4
+#define buttonPin2 3
+#define buttonPin1 2
 #define buzzer 12
 #define MPU_addr 0x68
 #define UpperThreshold 518
 #define LowerThreshold 490
 #define id 1
-#define protector 7
+#define protector 5
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 TinyGPSPlus gps;
-SoftwareSerial gpsSerial(2, 3);
+SoftwareSerial gpsSerial(7, 6);
 SoftwareSerial sim(10, 11);
 SoftwareSerial BTSerial(9, 8);
 
@@ -39,9 +39,11 @@ int count = 0;
 int check = 0;
 int flag = 0;
 bool condition = false;
-int buttonPushCounter[2] = { 2, 2 };
-int lastButtonState[2] = { 0, 0 };
 bool alert_cancel = false;
+int counter1 = 0;
+int counter2 = 0;
+int onetime = 0;
+
 enum State { F,
              L,
              B,
@@ -86,51 +88,45 @@ void mpu_read(void) {
   amplitude = raw_amplitude * 10;
 }
 
-void software_Reset() {
-  asm volatile("  jmp 0");
-}
 
 void check_activate() {
-  int currentState = digitalRead(buttonPin1);
-  if (lastButtonState[0] != currentState) {
-    lastButtonState[0] = currentState;
-    if (currentState == HIGH) {
-      buttonPushCounter[0]++;
-      if (buttonPushCounter[0] == 3) buttonPushCounter[0] = 1;
-      if (buttonPushCounter[0] == 1) {
-        condition = true;
-        lcd.clear();
-        lcd.print(F(" System is Off"));
-        while (condition == 1) {
-          check_activate();
-        }
-      } else {
-        software_Reset();
-      }
-    }
+  if (digitalRead(buttonPin1) == LOW) {
+    counter1++;
+    if (counter1 == 3) counter1 = 1;
+    if (counter1 == 1) condition = true;
+    if (counter1 == 2) asm volatile("  jmp 0");
   }
 }
+void check_button() {
+  if (digitalRead(buttonPin2) == LOW) {
+    counter2++;
+    if (counter2 == 3) counter2 = 1;
+    if (counter2 == 1) {
+      onetime = 1;
+      alert_cancel = true;
+    }
+    if (counter2 == 2) alert_cancel = false;
+  }
+}
+
+void callsms() {
+}
+
 void setup() {
-  Wire.setClock(4000000);
-  Wire.begin();
-  delay(250);
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);
-  Wire.write(0x00);
-  Wire.endTransmission();
   lcd.init();
   lcd.clear();
   lcd.backlight();
   lcd.print(F("     System"));
   lcd.setCursor(0, 1);
   lcd.print(F(" Initialization"));
-  delay(2000);
   Serial.begin(9600);
   gpsSerial.begin(9600);
   GpsInfo();
   BTSerial.begin(9600);
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin1), check_activate, FALLING);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin2), check_button, FALLING);
   pinMode(buzzer, OUTPUT);
   pinMode(protector, OUTPUT);
   lcd.clear();
@@ -139,75 +135,88 @@ void setup() {
   lcd.print(F("     System  "));
   sim.begin(9600);
   delay(1000);
+  Wire.setClock(4000000);
+  Wire.begin();
+  delay(250);
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
 }
 void loop() {
-  check_activate();
-  if ((millis() - previousMillis) >= 10) {
-    previousMillis = millis();
-    reading = analogRead(0);
-    if (reading > UpperThreshold && IgnoreReading == false) {
-      if (FirstPulseDetected == false) {
-        FirstPulseTime = millis();
-        FirstPulseDetected = true;
-      } else {
-        SecondPulseTime = millis();
-        PulseInterval = SecondPulseTime - FirstPulseTime;
-        FirstPulseTime = SecondPulseTime;
-      }
-      IgnoreReading = true;
-    }
-    if (reading < LowerThreshold && IgnoreReading == true) IgnoreReading = false;
-    if ((millis() - previousMillis2) >= 1000) {
-      previousMillis2 = millis();
-      BPM = (1.0 / PulseInterval) * 60.0 * 1000;
-    }
+  if (condition == 1) {
+    lcd.clear();
+    lcd.print(F("System is turned"));
+    lcd.setCursor(0, 1);
+    lcd.print(F("       off"));
+    while (1);
   }
-  int currentState2 = digitalRead(buttonPin2);
-  if (lastButtonState[1] != currentState2) {
-    lastButtonState[1] = currentState2;
-    if (currentState2 == HIGH) {
-      buttonPushCounter[1]++;
-      if (buttonPushCounter[1] == 3) buttonPushCounter[1] = 1;
-      if (buttonPushCounter[1] == 1) {
+  if (condition == 0) {
+    if (stateStr[9] == "Fall" && alert_cancel == 1 && onetime == 1) {
+      onetime = 0;
+      SendMessage(3);
+    }
+
+    if ((millis() - previousMillis) >= 10) {
+      previousMillis = millis();
+      reading = analogRead(0);
+      if (reading > UpperThreshold && IgnoreReading == false) {
+        if (FirstPulseDetected == false) {
+          FirstPulseTime = millis();
+          FirstPulseDetected = true;
+        } else {
+          SecondPulseTime = millis();
+          PulseInterval = SecondPulseTime - FirstPulseTime;
+          FirstPulseTime = SecondPulseTime;
+        }
+        IgnoreReading = true;
+      }
+      if (reading < LowerThreshold && IgnoreReading == true) IgnoreReading = false;
+      if ((millis() - previousMillis2) >= 1000) {
+        previousMillis2 = millis();
+        BPM = (1.0 / PulseInterval) * 60.0 * 1000;
+        lcd.clear();
+        lcd.print(String("State: ") + String(stateStr[9]));
+        lcd.setCursor(0, 1);
+        lcd.print(String("pulse: ") + String(BPM));
+      }
+    }
+
+    mpu_read();
+    if ((ax >= -1.30) && (ax <= 4.30) && (ay <= 2.50) && (ay >= -1.30) && (az <= 7.50) && (az >= -3.30)) st = N;
+    if ((ax >= -0.20) && (ax <= 0.38) && (ay <= -0.40) && (ay >= -0.88) && (az >= 0.26) && (az <= 0.88)) st = RB;
+    if ((ax >= -0.20) && (ax <= 0.38) && (ay <= -0.40) && (ay >= -0.88) && (az >= -0.88) && (az <= -0.26)) st = RF;
+    if ((ax >= -0.20) && (ax <= 0.38) && (ay <= 0.88) && (ay >= 0.40) && (az >= 0.26) && (az <= 0.88)) st = BL;
+    if ((ax >= -0.20) && (ax <= 0.38) && (ay <= 0.88) && (ay >= 0.40) && (az <= -0.26) && (az >= -0.88)) st = LF;
+    if ((ax >= -0.30) && (ax <= 0.30) && (ay >= 0.80) && (ay <= 1.20) && (az <= 0.56) && (az >= -0.56)) st = L;
+    if ((ax >= -0.30) && (ax <= 0.30) && (ay <= 0.68) && (ay >= -0.68) && (az <= 1.20) && (az >= 0.80)) st = B;
+    if ((ax >= -0.30) && (ax <= 0.30) && (ay <= 0.68) && (ay >= -0.68) && (az <= -0.80) && (az >= -1.20)) st = F;
+    if ((ax >= -0.30) && (ax <= 0.30) && (ay <= -0.80) && (ay >= -1.20) && (az <= 0.56) && (az >= -0.56)) st = R;
+    BTSerial.listen();
+    if (BTSerial.available() > 0) {
+      b = BTSerial.read();
+      if (b == 'S') {
         alert_cancel = true;
-        if (stateStr[9] == "Fall") SendMessage(3);
-      } else {
-        alert_cancel = false;
+        if (stateStr[9] == "Fall") SendMessage(2);
+        b = " ";
       }
     }
-  }
-  mpu_read();
-  if ((ax >= -1.30) && (ax <= 4.30) && (ay <= 2.50) && (ay >= -1.30) && (az <= 7.50) && (az >= -3.30)) st = N;
-  if ((ax >= 0.55) && (ax <= 0.88) && (ay <= 0.40) && (ay >= -0.11) && (az <= 0.92) && (az >= 0.48)) st = BL;
-  if ((ax >= -1.10) && (ax <= -0.30) && (ay <= 0.40) && (ay >= -0.11) && (az <= 1.10) && (az >= 0.16)) st = LF;
-  if ((ax >= 0.29) && (ax <= 1.10) && (ay <= 0.22) && (ay >= -0.11) && (az >= -0.88) && (az <= 1.10)) st = RB;
-  if ((ax >= -1.10) && (ax <= -0.29) && (ay <= 0.45) && (ay >= -0.20) && (az <= -0.26) && (az >= -1.10)) st = RF;
-  if ((ax <= 2.12) && (ax >= 0.73) && (ay <= 0.33) && (ay >= -0.22) && (az <= 0.54) && (az >= -0.26)) st = B;
-  if ((ax >= -0.33) && (ax <= 0.57) && (ay <= 0.41) && (ay >= -0.28) && (az <= 1.39) && (az >= 0.80)) st = L;
-  if ((ax >= -0.30) && (ax <= 0.47) && (ay <= 0.45) && (ay >= -0.13) && (az <= -0.84) && (az >= -1.55)) st = R;
-  if ((ax >= -1.40) && (ax <= -0.85) && (ay <= 0.46) && (ay >= -0.22) && (az <= 0.17) && (az >= -0.37)) st = F;
-  BTSerial.listen();
-  if (BTSerial.available() > 0) {
-    b = BTSerial.read();
-    if (b == 'S') {
-      alert_cancel = true;
-      buttonPushCounter[1] = 1;
-      if (stateStr[9] == "Fall") SendMessage(2);
-      b = " ";
+    if ((millis() - now) >= 200) {
+      now = millis();
+      String btsend = (String)id + "," + (String)stateStr[9] + "," + (String)BPM;
+      BTSerial.println(btsend);
     }
-  }
-  if ((millis() - now) >= 200) {
-    now = millis();
-    String btsend = (String)id + "," + (String)stateStr[9] + "," + (String)BPM;
-    BTSerial.println(btsend);
-  }
-  if (alert_cancel == 1) {
-    digitalWrite(buzzer, LOW);
+    if (alert_cancel == 1) {
+      digitalWrite(buzzer, LOW);
+      delay(50);
+    }
+
+    Serial.print(stateStr[st]);
+    Serial.println(F(" "));
+    time_check();
+    PREampl = amplitude;
     delay(50);
   }
-  time_check();
-  PREampl = amplitude;
-  delay(50);
 }
 
 
@@ -220,12 +229,13 @@ void GpsInfo() {
           latitude = String(gps.location.lat(), 6);
           logitude = String(gps.location.lng(), 6);
           gps_st = -1;
-          gpsSerial.end();
+          // gpsSerial.end();
         } else {
           delay(1000);
           ++gps_st;
         }
       }
+
   } while (gps_st != -1);
 }
 
@@ -252,12 +262,12 @@ void SendMessage(int m) {
   if (m == 3) {
     SMS = "The patient with an ID " + (String)id + " is alright" + "\r";
     lcd.clear();
-    lcd.print(F("  Patient cancel"));
+    lcd.print(F(" Patient cancel"));
     lcd.setCursor(0, 1);
     lcd.print(F("     Alert"));
   }
+  
   sim.listen();
-  delay(1000);
   sim.println("AT");
   updateSerial();
   sim.println("AT+CMGF=1");
@@ -268,8 +278,8 @@ void SendMessage(int m) {
   updateSerial();
   delay(500);
   sim.write(26);
-  delay(5000);
-  sim.end();
+  delay(10000);
+  // sim.end();
   gps_st = 0;
 }
 
@@ -296,8 +306,6 @@ void time_check() {
     check = 0;
     stateStr[9] = "normal";
     digitalWrite(protector, LOW);
-    lcd.clear();
-    lcd.print(F("  Normal State"));
   }
   if (((millis() - lastRefreshTime) >= 2000) && (flag == 1) && (lastRefreshTime != 0)) {
     if (check == 1 && count >= 6 && alert_cancel == 0) {
